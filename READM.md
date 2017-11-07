c853bb85b0037c35a22c759c3fdb9027ad274bef
shengbolou <loushengbo@hotmail.com>
2017-11-07 11:23:46 -0500

    add receive socket

--- READM.md
+++ READM.md
@@ -1,1 +1,3 @@
 # RawSocket in Python
+
+http://www-edlab.cs.umass.edu/cs653/a3.html
--- rawhttpget.py
+++ rawhttpget.py
@@ -12,15 +12,15 @@ def checksum(msg):
 
 
 packet = ''
- 
+
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
 except socket.error:
     sys.exit()
 
-source_ip = '192.168.1.101'
-dest_ip = '192.168.1.1' # or socket.gethostbyname('www.google.com')
+source_ip = '10.0.2.15'
+dest_ip = socket.gethostbyname("elsrv2.cs.umass.edu")
  
 # ip header fields
 ip_ihl = 5
@@ -63,7 +63,7 @@ tcp_flags = tcp_fin + (tcp_syn << 1) + (tcp_rst << 2) + (tcp_psh <<3) + (tcp_ack
 # the ! in the pack format string means network order
 tcp_header = pack('!HHLLBBHHH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window, tcp_check, tcp_urg_ptr)
  
-user_data = b'asdhakjshd'
+user_data = b'hello!'
 
 # pseudo header fields
 source_address = socket.inet_aton( source_ip )
@@ -84,4 +84,25 @@ tcp_header = pack('!HHLLBBH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_o
 packet = ip_header + tcp_header + user_data
  
 #Send the packet finally - the port specified has no effect
-s.sendto(packet, (dest_ip , 0 ))    # put this in a loop if you want to flood the target
+s.sendto(packet, (dest_ip , 0 ))
+
+try:
+    receiveSocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+except socket.error:
+    sys.exit()
+
+response = receiveSocket.recvfrom(65565)
+resPacket = response[0]
+iph = resPacket[0:20]
+tcp_data = resPacket[20:49]
+print(tcp_data.hex())
+
+seq_num = int.from_bytes(tcp_data[4:8],byteorder='big')
+ack_num = int.from_bytes(tcp_data[8:12],byteorder='big')
+
+
+
+
+
+
+
