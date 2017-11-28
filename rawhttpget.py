389affb2d951776bbf65fc1156114e13aa0412db
shengbolou <loushengbo@hotmail.com>
2017-11-28 14:51:22 -0500

    working on get

--- rawhttpget.py
+++ rawhttpget.py
@@ -55,7 +55,7 @@ def generate_ip_header():
     return ip_header
 
 
-def generate_tcp_header(seq, ack_seq, syn, ack, fin=0, data=""):
+def generate_tcp_header(seq, ack_seq, syn, ack, fin=0, rst=0, psh=0, data=""):
     # tcp header fields
     tcp_source = 1234  # source port
     tcp_dest = 80  # destination port
@@ -65,8 +65,8 @@ def generate_tcp_header(seq, ack_seq, syn, ack, fin=0, data=""):
     # tcp flags
     tcp_fin = fin
     tcp_syn = syn
-    tcp_rst = 0
-    tcp_psh = 0
+    tcp_rst = rst
+    tcp_psh = psh
     tcp_ack = ack
     tcp_urg = 0
     tcp_window = socket.htons(5840)  # maximum allowed window size
@@ -104,45 +104,53 @@ def generate_tcp_header(seq, ack_seq, syn, ack, fin=0, data=""):
     return tcp_header
 
 
-def validate_tcp_checksum(len, data):
-    tcp_length = len
-    tcp_data = data
+def validate_tcp_checksum(resPacket, data_len):
+    tcph = resPacket[20:]
+    tcp_h = unpack('!HHLLBBHHH' + str(data_len) + 's',tcph)
+    len = data_len+1 if data_len%2==1 else data_len
+    actual_tcph = pack('!HHLLBBHHH' + str(len) + 's',tcp_h[0],tcp_h[1],tcp_h[2],tcp_h[3],
+                       tcp_h[4],tcp_h[5],tcp_h[6],tcp_h[7],tcp_h[8],tcp_h[9])
     source_address = socket.inet_aton(source_ip)
     dest_address = socket.inet_aton(dest_ip)
     placeholder = 0
     protocol = socket.IPPROTO_TCP
-    psh = pack('!4s4sBBH', source_address, dest_address, placeholder, protocol, tcp_length)
-    psh = psh + tcp_data
-
-    return checksum(psh)==0
+    psh = pack('!4s4sBBH', source_address, dest_address, placeholder, protocol, data_len+20)
+    psh = psh + actual_tcph
+    return checksum(psh) == 0
 
 
 def receive():
     while(True):
-        resPacket = receiveSocket.recvfrom(65565)[0]
+        resPacket = receiveSocket.recvfrom(65565)
+        # print "raw"
+        # print resPacket
+        # print
+        resPacket = resPacket[0]
         iph = resPacket[0:20]
         source = socket.inet_ntoa(iph[12:16])
         if source == dest_ip and checksum(iph)==0:
-            tcp_length = int(resPacket[32].encode('hex'), 16)/4
-            tcp_data = resPacket[20:20+tcp_length]
-            if validate_tcp_checksum(tcp_length,tcp_data):
-                print(":".join("{:02x}".format(ord(c)) for c in iph))
-                print(":".join("{:02x}".format(ord(c)) for c in tcp_data))
+            data_len = int(iph[2:4].encode('hex'), 16)-40
+
+            print "correct length? %d" % data_len
+            print "formatted"
+            print(":".join("{:02x}".format(ord(c)) for c in resPacket))
+            print
+            print validate_tcp_checksum(resPacket, data_len)
+            if validate_tcp_checksum(resPacket, data_len):
                 return resPacket
 
 
 def receive_ack(seq):
     resPacket = receive()
-    tcp_length = int(resPacket[32].encode('hex'), 16) / 4
-    tcp_data = resPacket[20:20 + tcp_length]
-    tcp_flag = int(tcp_data[12:14].encode('hex'), 16)&16
+    tcp_header = resPacket[20:40]
+    tcp_flag = int(tcp_header[12:14].encode('hex'), 16) & 16
     if tcp_flag==16:
-        recv_seq_num = int(tcp_data[4:8].encode('hex'), 16)
-        recv_ack_num = int(tcp_data[8:12].encode('hex'), 16)
+        recv_seq_num = int(tcp_header[4:8].encode('hex'), 16)
+        recv_ack_num = int(tcp_header[8:12].encode('hex'), 16)
         if recv_ack_num == seq+1:
-            return recv_seq_num,resPacket
+            return recv_seq_num, resPacket
         else:
-            return False,resPacket
+            return False, resPacket
 
 def initialize_conn():
     ip_header = generate_ip_header()
@@ -152,51 +160,87 @@ def initialize_conn():
     sendSocket.sendto(packet, (dest_ip, 0))
 
     ack, resPacket = receive_ack(454)
-    print ack
 
     if ack!=False:
         ip_header = generate_ip_header()
         tcp_header = generate_tcp_header(455, ack+1, 0, 1)
         packet = ip_header + tcp_header
         sendSocket.sendto(packet, (dest_ip, 0))
-        return ack, 456
+        return ack, 455
     else:
         print "conn error!"
 
-def terminate():
+
+def reset():
     ip_header = generate_ip_header()
-    tcp_header = generate_tcp_header(454, 0, 0, 0, fin=1)
+    tcp_header = generate_tcp_header(454, 0, 0, 0, rst=1)
     packet = ip_header + tcp_header
 
     sendSocket.sendto(packet, (dest_ip, 0))
 
-    ack, resPacket = receive_ack(454)
-    print ack
+def terminate(seq,ack):
+    ip_header = generate_ip_header()
+    tcp_header = generate_tcp_header(seq, ack+1, 0, 1, fin=1)
+    packet = ip_header + tcp_header
 
-    if ack!=False:
+    sendSocket.sendto(packet, (dest_ip, 0))
+
+    recack, resPacket = receive_ack(seq) # receive ack
+
+    if recack != False:
+        recack, resPacket = receive_ack(seq) # receive syn-ack
+
         ip_header = generate_ip_header()
-        tcp_header = generate_tcp_header(455, ack+1, 0, 1)
+        tcp_header = generate_tcp_header(seq+1, recack+1, 0, 1)
         packet = ip_header + tcp_header
         sendSocket.sendto(packet, (dest_ip, 0))
-        return ack, 456
     else:
         print "terminate error!"
 
 def send_get(ack, seq):
     get_message = "GET / HTTP/1.0\r\nHost: elsrv2.cs.umass.edu\r\n\r\n"
     ip_header = generate_ip_header()
-    tcp_header = generate_tcp_header(seq, ack+1, 0, 1, data=get_message)
+    tcp_header = generate_tcp_header(seq, ack+1, 0, 1, psh=1, data=get_message)
     packet = ip_header+tcp_header
-
     sendSocket.sendto(packet, (dest_ip, 0))
 
-    ack, resPacket = receive_ack(seq)
-    return ack,resPacket
+def receive_file_and_terminate():
+    data = ""
+    while(True):
+        resPacket = receive()
+        iph = resPacket[0:20]
+        data_len = int(iph[2:4].encode('hex'), 16)-40
+        print "data length: %d" % data_len
+        tcp_header = resPacket[20:40]
+        tcp_flag = int(tcp_header[12:14].encode('hex'), 16) & 25
+        print tcp_flag
+        if tcp_flag==25: # fin-psh-ack, terminate
+            recv_seq_num = int(tcp_header[4:8].encode('hex'), 16)
+            recv_ack_num = int(tcp_header[8:12].encode('hex'), 16)
+            ip_header = generate_ip_header()
+            tcp_header = generate_tcp_header(recv_ack_num, recv_seq_num+data_len, 0, 1)
+            packet = ip_header + tcp_header
+
+            sendSocket.sendto(packet, (dest_ip, 0))
+
+            return data
+        if data_len>0: # get the data
+            data = unpack("!"+str(data_len)+"s", resPacket[40:40+data_len])
+
+
 
 ack, seq = initialize_conn()
-new_ack = send_get(ack, seq)
-terminate()
+send_get(ack, seq)
+data = receive_file_and_terminate()
+# print data
+data = data[0]
+idx = data.find("\r\n\r\n")
+f = open('test.html','wb')
+f.write(data[idx+4:])
+f.close()
 
+
+# reset()
 
 
 
